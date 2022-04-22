0b9e9cfa8d68d40afb01d5fde43f2b48c5a0597c
lucaslucenak <lucas.lucenak@gmail.com>
2022-04-22 20:36:11 -0300

    added post method

--- index.js
+++ index.js
@@ -4,17 +4,26 @@ const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.render('../views/home.ejs')
 })
+
+app.post('/createCinema', (req, res) => {
+    res.send('Form recebido')
+})
+
 app.get('/wishList', (req, res) => {
     res.render('../views/wishList.ejs')
 })
-app.listen(8085, function (err) {
+
+app.listen(8089, function (err) {
     if (err) {
         console.error(err)
     }
     else {
-        console.log('listening on port http://localhost:8085')
+        console.log('listening on port http://localhost:8089')
     }
 })
