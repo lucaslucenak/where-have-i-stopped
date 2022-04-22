f0a246521e3b7b2a135303004eeae731c26ad578
lucaslucenak <lucas.lucenak@gmail.com>
2022-04-22 19:56:44 -0300

    updated home form

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const express = require('express');
 app = express();
 const bodyParser = require('body-parser');
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send('Main page')
+    res.render('../views/home.ejs')
 })
 app.get('/wishList', (req, res) => {
-    res.send('WishList')
+    res.render('../views/wishList.ejs')
 })
-app.listen(8083, function (err) {
+app.listen(8085, function (err) {
     if (err) {
         console.error(err)
     }
     else {
-        console.log('listening on port http://localhost:8083')
+        console.log('listening on port http://localhost:8085')
     }
 })
