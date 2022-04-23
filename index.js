449e3615b3cb85a66f2172daa91d3695ca7832d9
lucaslucenak <lucas.lucenak@gmail.com>
2022-04-22 22:53:08 -0300

    add wish feature

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 app = express();
 const bodyParser = require('body-parser');
 const tb_cinemas = require('./database/models/tb_cinemas')
+const tb_wishLists = require('./database/models/tb_wishLists')
 
 app.set('view engine', 'ejs');
 
@@ -29,11 +30,19 @@ app.get('/wishList', (req, res) => {
     res.render('../views/wishList.ejs')
 })
 
-app.listen(8089, function (err) {
+app.post('/createWish', (req, res) => {
+    var name = req.body.name;
+    var link = req.body.link;
+    tb_wishLists.create({name: name, link: link}).then(() => {
+        res.redirect('/');
+    });
+});
+
+app.listen(8086, function (err) {
     if (err) {
         console.error(err)
     }
     else {
-        console.log('listening on port http://localhost:8089')
+        console.log('listening on port http://localhost:8086')
     }
 })
